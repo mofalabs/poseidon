0327b1a543c66a1621f6022cf8deccc073b45677
sky <t19930456@gmail.com>
2024-01-24 23:09:25 +0800

    refactor: poseidon export

--- lib/poseidon.dart
+++ lib/poseidon.dart
@@ -1,55 +1,18 @@
 library poseidon;
 
-import 'constants/p1.dart';
-import 'constants/p2.dart';
-import 'constants/p3.dart';
-import 'constants/p4.dart';
-import 'constants/p5.dart';
-import 'constants/p6.dart';
-import 'constants/p7.dart';
-import 'constants/p8.dart';
-import 'constants/p9.dart';
-import 'constants/p10.dart';
-import 'constants/p11.dart';
-import 'constants/p12.dart';
-import 'constants/p13.dart';
-import 'constants/p14.dart';
-import 'constants/p15.dart';
-import 'constants/p16.dart';
-
-const poseidonNumToHashFN = [
-  poseidon1,
-  poseidon2,
-  poseidon3,
-  poseidon4,
-  poseidon5,
-  poseidon6,
-  poseidon7,
-  poseidon8,
-  poseidon9,
-  poseidon10,
-  poseidon11,
-  poseidon12,
-  poseidon13,
-  poseidon14,
-  poseidon15,
-  poseidon16,
-];
-
-BigInt poseidonHash(List<BigInt> inputs) {
-  try {
-    if (inputs.length <= poseidonNumToHashFN.length) {
-      Function hashFN = poseidonNumToHashFN[inputs.length - 1];
-      return hashFN(inputs);
-    } else if (inputs.length <= 32) {
-      List<BigInt> hash1 = inputs.sublist(0, 16);
-      List<BigInt> hash2 = inputs.sublist(16);
-      return poseidonHash([poseidonHash(hash1), poseidonHash(hash2)]);
-    } else {
-      throw Exception(
-          'Yet to implement: Unable to hash a vector of length ${inputs.length}');
-    }
-  } catch (e) {
-    throw Exception('poseidonHash error: $e');
-  }
-}
+export 'constants/p1.dart';
+export 'constants/p2.dart';
+export 'constants/p3.dart';
+export 'constants/p4.dart';
+export 'constants/p5.dart';
+export 'constants/p6.dart';
+export 'constants/p7.dart';
+export 'constants/p8.dart';
+export 'constants/p9.dart';
+export 'constants/p10.dart';
+export 'constants/p11.dart';
+export 'constants/p12.dart';
+export 'constants/p13.dart';
+export 'constants/p14.dart';
+export 'constants/p15.dart';
+export 'constants/p16.dart';
